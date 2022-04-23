b9cd0613e93ad09803628f64f1eac2ddabd99d0a
Laurent Andriamifidy <lolrenx@gmail.com>
2022-04-23 12:25:56 +0200

    django environ

--- src/website/settings.py
+++ src/website/settings.py
@@ -1,17 +1,30 @@
+import os
 from pathlib import Path
+
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#(^qb8&3r_+0q18w12e=z$2p4!i)ehdzspt96@3+izjkibx(q%"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# False if not in os.environ because of casting above
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS: list[str] = []
 
